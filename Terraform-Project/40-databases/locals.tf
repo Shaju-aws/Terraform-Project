4095c33ff31d91534dd8c424e5d6fcdc74936f61
shajugit <v.shajahan1996@gmail.com>
2026-07-05 14:01:00 +0530

    ansible

--- Terraform-Project/40-databases/locals.tf
+++ Terraform-Project/40-databases/locals.tf
@@ -1,6 +1,7 @@
 locals {
     database_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     database_subnet_ids = data.aws_ssm_parameter.database_subnet_ids.value
+    database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
 }
 
 locals {
