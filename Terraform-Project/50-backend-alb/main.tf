8c53a8cf5b972377d63c497b4fb86ff36884c071
shajugit <v.shajahan1996@gmail.com>
2026-07-12 19:08:28 +0530

    ansible

--- Terraform-Project/50-backend-alb/main.tf
+++ Terraform-Project/50-backend-alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "backend-alb" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_sg_id]
-  subnets            = local.private_subnet_id
+  subnets            = local.private_subnet_ids
 
 
   tags = merge(
